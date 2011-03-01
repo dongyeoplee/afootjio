5a0c4dc5db7a81e4aed8f63cbd4ea610e1285443
afootjio <dongyeop.lee@headflow.net>
2011-03-01 16:23:25 +0900

    Improved the README file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Afootjio::Application.config.secret_token = 'e94b1a43bfb96db3c51640a560779567e8c23d715c50da6bde6e9b971ca07f460047b9e7a322cd16cbace90df96cefa24f6ab9e167d09bef1b628e65072a5872'
